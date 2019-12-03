7bf99570bdd53770a300f06e4504d78573ef81d2
Scott Wehrenberg <scott@wehrenberg.us>
2019-12-02 23:41:08 -0500

    Recursive implementation for total_fuel

--- 2019/day1/__init__.py
+++ 2019/day1/__init__.py
@@ -5,23 +5,27 @@ def fuel(x):
     return math.floor(int(x)/3) - 2
 
 def total_fuel(x):
-    sum = -1 * x 
+    total = -1 * x 
     while x > 0:
-        sum += x
+        total += x
         x = fuel(x)
 
-    return sum
+    return total
+
+def total_fuel_recursive(x, total=0):
+    additional = fuel(x)
+    if additional > 0:
+        return total_fuel_recursive(additional, additional+total)
+
+    return total
 
 
 def main(session):
     raw = common.load_input(1, session)
-    lines = map(int, raw.splitlines())
+    lines = list(map(int, raw.splitlines()))
 
-    part1 = 0
-    part2 = 0
-    for line in lines:
-        part1 += fuel(line)
-        part2 += total_fuel(line)
+    part1 = sum(map(fuel, lines))
+    part2 = sum(map(total_fuel_recursive, lines))
 
     print(f"Part 1: {part1}")
     print(f"Part 2: {part2}")
