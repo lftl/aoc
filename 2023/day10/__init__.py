35f2de112dd588083634fb08d2fee3af0bb64708
Scott Wehrenberg <scott@wehrenberg.us>
2023-12-10 12:21:40 -0500

    Remove cruft

--- 2023/day10/__init__.py
+++ 2023/day10/__init__.py
@@ -185,9 +185,6 @@ def get_neighbors(pos, data):
             neighbors.append((row, col))
     return neighbors
 
-def get_distance(pos1, pos2):
-    return abs(pos1[0] - pos2[0]) + abs(pos1[1] - pos2[1])
-
 """
 --- Part Two ---
 
